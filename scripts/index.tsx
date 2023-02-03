d267833b59fe481ae32070847d99977020b69aeb
Lagicrus <Lagicrus@users.noreply.github.com>
2023-02-03 20:03:26 +0000

    Init

---
+++ scripts/index.tsx
@@ -0,0 +1,7 @@
+import "./wdyr";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-700.css";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
